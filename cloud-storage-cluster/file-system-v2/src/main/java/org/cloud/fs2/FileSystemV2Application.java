264b2e74ce810e2de5bc0b335589ea464be67b96
ember <laiqw6537@qq.com>
2025-08-05 00:27:31 +0800

    start file-system-v2

---
+++ cloud-storage-cluster/file-system-v2/src/main/java/org/cloud/fs2/FileSystemV2Application.java
@@ -0,0 +1,13 @@
+package org.cloud.fs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSystemV2Application.class, args);
+    }
+
+}
---
+++ cloud-storage-cluster/file-system-v2/src/test/java/org/cloud/fs2/FileSystemV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cloud.fs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSystemV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
